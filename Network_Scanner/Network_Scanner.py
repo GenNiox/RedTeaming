8b6808849d33fef5da53b7f24c4e3df737cd4b5f
GenNiox <75768603+GenNiox@users.noreply.github.com>
2023-02-18 17:42:56 -0700

    Add files via upload

--- Network_Scanner/Network_Scanner.py
+++ Network_Scanner/Network_Scanner.py
@@ -21,7 +21,7 @@ def scan(ip_address, subnet_mask):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]  # srp allows custom addresses.
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
     response_list = []
     for answer in answered_list:
