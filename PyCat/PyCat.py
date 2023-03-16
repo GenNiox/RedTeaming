4b9babe3a5e349eeba3c2b1af5f1ec9454e0a0b7
GenNiox <75768603+GenNiox@users.noreply.github.com>
2023-03-15 23:08:01 -0600

    Fixed indentation of PyCat class.

--- PyCat/PyCat.py
+++ PyCat/PyCat.py
@@ -43,74 +43,74 @@ class PyCat:
         else:
             self.send()
 
-def send(self):
-    self.socket.connect((self.args.target, self.args.port))
-    if self.buffer:
-        self.socket.send(self.buffer)
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
 
-        try:
-            while True:
-                recv_len = 1
-                response = ""
-                while recv_len:
-                    data = self.socket.recv(4096)
-                    recv_len = len(data)
-                    response += data.decode()
-                    if recv_len < 4096:
-                        break
-                    if response:
-                        print(response)
-                        buffer = input("> ")
-                        buffer += "\n"
-                        self.socket.send(buffer.encode())
-        except KeyboardInterrupt:
-            print("[~] User terminated session.")
-            print("[+] Exiting..")
-            self.socket.close()
-            sys.exit()
-
-
-def listen(self):
-    self.socket.bind((self.args.target, self.args.port))
-    self.socket.listen(5)
-    while True:
-        client_socket, _ = self.socket.accept()
-        client_thread = threading.Thread(target=self.handle, args=(client_socket,))
-        client_thread.start()
-
-
-def handle(self, client_socket):
-    if self.args.execute:
-        output = execute(self.args.execute)
-        client_socket.send(output.encode())
-    elif self.args.upload:
-        file_buffer = b""
-        while True:
-            data = client_socket.recv(4096)
-            if data:
-                file_buffer += data
-            else:
-                break
-        with open(self.args.upload, "wb") as f:
-            f.write(file_buffer)
-        message = f"Saved file {self.args.upload}"
-        client_socket.send(message.encode())
-
-    elif self.args.command:
-        cmd_buffer = b""
-        while True:
             try:
-                client_socket.send(b"PyCat: #> ")
-                while "\n" not in cmd_buffer.decode():
-                    cmd_buffer += client_socket.recv(64)
-                response = execute(cmd_buffer.decode())
-                if response:
-                    client_socket.send(response.decode())
-                cmd_buffer = b""
-            except Exception as e:
-                print(f"[-] Server killed: {e}")
+                while True:
+                    recv_len = 1
+                    response = ""
+                    while recv_len:
+                        data = self.socket.recv(4096)
+                        recv_len = len(data)
+                        response += data.decode()
+                        if recv_len < 4096:
+                            break
+                        if response:
+                            print(response)
+                            buffer = input("> ")
+                            buffer += "\n"
+                            self.socket.send(buffer.encode())
+            except KeyboardInterrupt:
+                print("[~] User terminated session.")
+                print("[+] Exiting..")
                 self.socket.close()
                 sys.exit()
+
+
+    def listen(self):
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(target=self.handle, args=(client_socket,))
+            client_thread.start()
+
+
+    def handle(self, client_socket):
+        if self.args.execute:
+            output = execute(self.args.execute)
+            client_socket.send(output.encode())
+        elif self.args.upload:
+            file_buffer = b""
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                else:
+                    break
+            with open(self.args.upload, "wb") as f:
+                f.write(file_buffer)
+            message = f"Saved file {self.args.upload}"
+            client_socket.send(message.encode())
+
+        elif self.args.command:
+            cmd_buffer = b""
+            while True:
+                try:
+                    client_socket.send(b"PyCat: #> ")
+                    while "\n" not in cmd_buffer.decode():
+                        cmd_buffer += client_socket.recv(64)
+                    response = execute(cmd_buffer.decode())
+                    if response:
+                        client_socket.send(response.decode())
+                    cmd_buffer = b""
+                except Exception as e:
+                    print(f"[-] Server killed: {e}")
+                    self.socket.close()
+                    sys.exit()
 
 
 if __name__ == '__main__':
