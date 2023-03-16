1dc9b5131920493cfd2a526b3b82fab0bb9d30f2
GenNiox <75768603+GenNiox@users.noreply.github.com>
2023-03-16 00:17:37 -0600

    Add files via upload

--- TCP_Server_and_Client/TCP_Client.py
+++ TCP_Server_and_Client/TCP_Client.py
@@ -18,7 +18,7 @@ while True:
             print(message)
         else:
             message = sys.stdin.readline()
-            server.send(message)
+            server.send(bytes(message))
             sys.stdout.write("<You>")
             sys.stdout.write(message)
             sys.stdout.flush()
